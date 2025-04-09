f91faeb8b1382920bf4a2c1920667fd48ba57718
berkcanemre <berkcan.emre.07@gmail.com>
2025-04-09 12:01:54 -0400

    completed ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("ShoppingList: ground meat, onion, oregano, pepper, garlic, flour, tomato, water, yogurt, ice");
+}
+}
